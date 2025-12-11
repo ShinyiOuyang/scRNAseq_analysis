02d9a9b23590640d95668feab5ee5a685a120f66
ShinyiOuyang <ouyang.shinyi@gmail.com>
2025-12-10 16:20:26 -0800

    add python script

---
+++ get_chrX_reads.py
@@ -0,0 +1,60 @@
+# Python script
+# Extract the X chromosome
+# Replace the suffix of the barcode (-1) with the data modality (RNA or ATAC)
+# Renaming the barcode is actually unnecessary in this case, but it will be necessary if you want to merge multiple runs of the same sample.
+# Therefore, we are changing the barcode name here as an example.
+
+import pysam
+import pandas as pd
+import subprocess
+
+bam_files = []
+DIR = "../HRR1795847/"
+curr_file = "possorted_genome_bam.bam"
+BAM_LIST = [
+    "possorted_genome_bam.bam"
+]
+# BAM_LIST = [
+#     "pbmc_granulocyte_sorted_10k_gex_possorted_bam",
+#     "pbmc_granulocyte_sorted_10k_atac_possorted_bam",
+# ]
+
+
+for BAM in BAM_LIST:
+    input_bam = DIR + BAM + ".bam"
+    output_bam = BAM + ".barcode.renamed.X.bam"
+    bam_files.append(output_bam)
+
+    in_bam = pysam.AlignmentFile(input_bam, "rb")
+    out_bam = pysam.AlignmentFile(output_bam, "wb", template=in_bam)
+
+    if BAM == "pbmc_granulocyte_sorted_10k_gex_possorted_bam":
+        for aln in in_bam.fetch("chrX"):
+            if aln.has_tag("CB"):
+                old_bc = aln.get_tag("CB")
+                new_bc = old_bc.replace("-1", "-RNA")
+                aln.set_tag("CB", new_bc)
+                out_bam.write(aln)
+
+    elif BAM == "pbmc_granulocyte_sorted_10k_atac_possorted_bam":
+        for aln in in_bam.fetch("chrX"):
+            if aln.has_tag("CB"):
+                old_bc = aln.get_tag("CB")
+                new_bc = old_bc.replace("-1", "-ATAC")
+                aln.set_tag("CB", new_bc)
+                out_bam.write(aln)
+
+    in_bam.close()
+    out_bam.close()
+
+subprocess.run(
+    [
+        "samtools",
+        "sort",
+        "-o",
+        "possorted_genome_bam.sorted.bam",
+        curr_file,
+    ],
+    check=True,
+)
+subprocess.run(["samtools", "index", "possorted_genome_bam.sorted.bam"], check=True)
