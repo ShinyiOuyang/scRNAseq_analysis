41328c3557c19b921650172a35f28db85db45840
ShinyiOuyang <ouyang.shinyi@gmail.com>
2025-12-16 12:42:52 -0800

    adjust pipeline

--- src/scLinaX_pipeline.sh
+++ src/scLinaX_pipeline.sh
@@ -5,9 +5,10 @@ set -e
 # Following steps from https://ytomofuji.github.io/scLinaX/articles/scLinaX_preprocessing_example.html
 
 # Step 1: Data Preparation
-python3 ./get_chrX_reads.py $1 # Extract chrX reads from BAM
+#python3 ./get_chrX_reads.py $1 # Extract chrX reads from BAM
 
 # Unzip filtered feature barcode matrix
+mkdir $1/filtered_feature_bc_matrix/
 tar -xf $1/filtered_feature_bc_matrix.tar.gz -C $1/filtered_feature_bc_matrix/
 zcat $/1filtered_feature_bc_matrix/barcodes.tsv.gz > $1/filtered_feature_bc_matrix/barcodes.tsv
 
