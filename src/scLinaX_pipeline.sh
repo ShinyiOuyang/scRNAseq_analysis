1009bdb0a10bed0a7ae9284b399d5a56b1e8903e
ShinyiOuyang <ouyang.shinyi@gmail.com>
2025-12-16 12:53:01 -0800

    adjusted filepaths

--- src/scLinaX_pipeline.sh
+++ src/scLinaX_pipeline.sh
@@ -5,7 +5,7 @@ set -e
 # Following steps from https://ytomofuji.github.io/scLinaX/articles/scLinaX_preprocessing_example.html
 
 # Step 1: Data Preparation
-python3 ./get_chrX_reads.py $1 # Extract chrX reads from BAM
+python3 ./src/get_chrX_reads.py $1 # Extract chrX reads from BAM
 
 # Unzip filtered feature barcode matrix
 if [ ! -d "$1/filtered_feature_bc_matrix/" ]; then 
@@ -15,10 +15,10 @@ tar -xf $1/filtered_feature_bc_matrix.tar.gz -C $1/filtered_feature_bc_matrix/
 zcat $1/filtered_feature_bc_matrix/barcodes.tsv.gz > $1/filtered_feature_bc_matrix/barcodes.tsv
 
 # Step 2: Run Cellsnp-lite
-bash ./cellsnp_lite.sh $1 $2 
+bash ./src/cellsnp_lite.sh $1 $2 
 
 # Step 3: Run Annovar
-bash ./annovar.sh
+bash ./src/annovar.sh
 
 # Step 4: QC
-Rscript RCODE_qc.R $1
+Rscript ./src/RCODE_qc.R $1
