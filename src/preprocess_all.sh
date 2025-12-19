aa2a4386fc9caf60797b62f936816e5335f1a732
ShinyiOuyang <ouyang.shinyi@gmail.com>
2025-12-19 11:41:58 -0800

    adjusted script

--- src/preprocess_all.sh
+++ src/preprocess_all.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-# Define an array of file names
-#files=("HRR1795848" "HRR1795849" "HRR1795850" "HRR1795851" "HRR1795852" "HRR1795853" "HRR1795854" "HRR1795855" "HRR1795856")
-samples=("HRR1795848")
+# Define an array of sample names
+samples=("HRR1795849" "HRR1795850" "HRR1795851" "HRR1795852" "HRR1795853" "HRR1795854" "HRR1795855" "HRR1795856")
 
 # Iterate over the array
 for sample in "${samples[@]}"; do
@@ -17,5 +16,9 @@ for sample in "${samples[@]}"; do
     samtools view -b -h /data/YH/Graves_dataset/CellRanger_results/${sample}/possorted_genome_X.sorted.bam chrX:79171491-79171491 -o to_scp/${sample}/${sample}_s162p_subset.bam
 
     cp /data/YH/Graves_dataset/CellRanger_results/${sample}/possorted_genome_X.sorted.bam to_scp/${sample}/${sample}_possorted_genome_X.sorted.bam
+
+    samtools index to_scp/${sample}/${sample}_possorted_genome_X.sorted.bam
+
+    samtools index to_scp/${sample}/${sample}_s162p_subset.bam
 
 done
