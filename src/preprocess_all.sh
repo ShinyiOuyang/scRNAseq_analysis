7aff5bea0abd204ed3a94ce7244b76c1981990c1
ShinyiOuyang <ouyang.shinyi@gmail.com>
2025-12-19 11:17:05 -0800

    modify preprocess_all

--- src/preprocess_all.sh
+++ src/preprocess_all.sh
@@ -3,11 +3,19 @@
 set -e
 
 # Define an array of file names
-files=("HRR1795848" "HRR1795849" "HRR1795850" "HRR1795851" "HRR1795852" "HRR1795853", "HRR1795854", "HRR1795855", "HRR1795856")
+#files=("HRR1795848" "HRR1795849" "HRR1795850" "HRR1795851" "HRR1795852" "HRR1795853" "HRR1795854" "HRR1795855" "HRR1795856")
+samples = ("HRR1795848")
 
 # Iterate over the array
-for file in "${files[@]}"; do
-    echo "Processing $file"
+for sample in "${samples[@]}"; do
+    echo "Processing $sample"
 
-    echo src/scLinaX_pipeline.sh /data/YH/Graves_dataset/CellRanger_results_SO_copy/${file} ${file}
+    echo src/scLinaX_pipeline.sh /data/YH/Graves_dataset/CellRanger_results_SO_copy/${sample} ${sample}
+
+    echo mkdir $sample
+
+    echo samtools view -b -h /data/YH/Graves_dataset/CellRanger_results_SO_copy/${sample}/possorted_genome_X.sorted.bam chrX:79171491-79171491 -o ${sample}/${sample}_s162p_subset.bam
+
+    echo cp /data/YH/Graves_dataset/CellRanger_results_SO_copy/${sample}/possorted_genome_X.sorted.bam ${sample}/${sample}_possorted_genome_X.sorted.bam
+
 done
