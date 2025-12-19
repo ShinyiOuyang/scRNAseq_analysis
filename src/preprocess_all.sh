1a98bb62dd3fae599842ce951819b4211af63f56
ShinyiOuyang <ouyang.shinyi@gmail.com>
2025-12-18 17:34:34 -0800

    update preprocess script

--- src/preprocess_all.sh
+++ src/preprocess_all.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-bash src/scLinaX_pipeline.sh /data/CellRanger_results_SO_copy/HRR1795892 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795892
+bash src/scLinaX_pipeline.sh /data/YH/Graves_dataset/CellRanger_results_SO_copy/HRR1795892 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795892
 
-bash src/scLinaX_pipeline.sh /data/CellRanger_results_SO_copy/HRR1795893 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795893
+bash src/scLinaX_pipeline.sh /data/YH/Graves_dataset/CellRanger_results_SO_copy/HRR1795893 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795893
 
-bash src/scLinaX_pipeline.sh /data/CellRanger_results_SO_copy/HRR1795894 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795894
+bash src/scLinaX_pipeline.sh /data/YH/Graves_dataset/CellRanger_results_SO_copy/HRR1795894 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795894
 
-bash src/scLinaX_pipeline.sh /data/CellRanger_results_SO_copy/HRR1795895 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795895
+bash src/scLinaX_pipeline.sh /data/YH/Graves_dataset/CellRanger_results_SO_copy/HRR1795895 /data/YH/Graves_dataset/CellRanger_results_SO_copy/refdata-gex-GRCh38-2024-A/fasta/genome.fa HRR1795895
